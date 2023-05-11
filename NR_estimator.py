878da735d7fc2f1381382fd05fd1433236e78086
Liyang Miao <umata@mail.ustc.edu.cn>
2023-05-11 16:17:16 +0800

    Some modification since late Apr.

---
+++ NR_estimator.py
@@ -0,0 +1,26 @@
+import numpy as np
+import NPCF_estimator as Ne
+
+
+
+def N_estimator(l1, l2, l3, Data_catalog, Random_catalog, ):
+    
+    DDD = Ne.Estimator(l1, l2, l3, )
+    
+    DDR = 
+    
+    DRD = 
+    
+    RDD = 
+    
+    RRD = 
+    
+    RDR = 
+    
+    DRR = 
+    
+    RRR = 
+    
+    return result
+    
+def R_estimator(l1, l2, l3, Random_catalog, bin_index)
